f12bcd88babc087334a298089d138e228158c34a
FabioJrDeEV <fj453578@gmail.com>
2025-06-14 11:09:51 -0300

    feat: adicionado metodo de post para adicionar tarefas

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,4 +1,37 @@
+import { useState } from "react";
+
 function Home() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  async function addTask() {
+    const dados = { title, description };
+    try {
+      if (title !== "" && description !== "") {
+        const resposta = await fetch(
+          "https://api-nodejs-crud.onrender.com/tasks",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(dados),
+          }
+        );
+
+        await resposta.json();
+        console.log("Dados enviado com sucesso");
+        setTitle("");
+        setDescription("");
+      } else {
+        alert("Por favor, preencha todos os campos!");
+      }
+    } catch (err) {
+      console.log(err);
+      console.log("Erro ao enviar dados");
+    }
+  }
+
   return (
     <div className="container">
       <h1 className="text-center mt-4 text-uppercase fs-3">
@@ -9,19 +42,27 @@ function Home() {
           className="w-75 mt-2 p-3 rounded-3"
           style={{ border: "1px solid black" }}
         >
-          <form onSubmit={(e) => e.preventDefault()}>
+          <form onSubmit={(e) => e.preventDefault(e)}>
             <input
               className="form-control mb-3"
               type="text"
               placeholder="Titulo da Terefa"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
             />
             <input
               className="form-control mb-3"
               type="text"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
               placeholder="Digite a Terefa"
             />
             <div className="d-flex justify-content-center">
-              <button type="button" class="btn btn-success">
+              <button
+                type="button"
+                onClick={addTask}
+                className="btn btn-success"
+              >
                 Adicionar Tarefa
               </button>
             </div>
@@ -29,9 +70,6 @@ function Home() {
         </div>
       </div>
       {/*Abaixo vai ser o local onde sera adicionado as tasks*/}
-      <div className="w-100 d-flex justify-content-center mt-4">
-        <h1>asuidaiu</h1>
-      </div>
     </div>
   );
 }
